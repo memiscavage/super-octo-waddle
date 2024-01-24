1ae2605b00bf323e9d87448e61481434c0399d75
Mariel Miscavage <memiscavage@gmail.com>
2024-01-23 19:58:03 -0600

    added vars for start button and header and quiz questions, also created event listener for start button and linked JS to the HTML

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,12 @@
 // Create function for timer
+
+//
+var startBtn = document.querySelector(".start-btn");
+var quizHeader = document.querySelector(".header");
+var quizQuestions = document.querySelector("#quiz-questions");
+
+//add event listener to access quiz
+startBtn.addEventListener("click", function (){
+    quizHeader.classList.add("hidden");
+    quizQuestions.classList.remove("hidden");
+})
